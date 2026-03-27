8cc12edbe3ac86dba94b186d3838f16edd50bfac
Faheem Anis <faheem@cartzlink.com>
2026-03-27 18:16:18 +0500

    Remove data type requirement for lazy components

--- src/ApplicationInterface.php
+++ src/ApplicationInterface.php
@@ -4,12 +4,12 @@ namespace framework\contracts;
 
 interface ApplicationInterface
 {
-    public function run($method);
+    public function run();
 
     /**
      * Register a component in the container
      */
-    public function registerComponent(string $name, ComponentInterface $component): void;
+    public function registerComponent(string $name, $component): void;
 
     public function registerExtension(string $name, ExtensionInterface $extension): void;
 }
