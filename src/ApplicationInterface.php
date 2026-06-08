a7fc8bf0a5b43db19a6d7a9c40c9b1006f50ee0d
Faheem Anis <faheem@cartzlink.com>
2026-06-08 18:59:18 +0500

    Updated application interface to align with module system

--- src/ApplicationInterface.php
+++ src/ApplicationInterface.php
@@ -11,5 +11,33 @@ interface ApplicationInterface
      */
     public function registerComponent(string $name, $component): void;
 
-    public function registerExtension(string $name, ExtensionInterface $extension): void;
+    /**
+     * Attempts to automatically scan and install all modules
+     * available within the current scope
+     * @return void
+     */
+    public function scanModules();
+
+    /**
+     * These methods provide boilerplates for registration of routes, controllers and views
+     */
+
+    /**
+     * Registers all routes within the specified folder
+     * 
+     * The specified folder must contain a "routes.php" file that exports a single `$router` variable.
+     * The variable will be mounted on the app.
+     * @param string $dir The path to the folder
+     * @return void
+     */
+    public function registerRoutes(string $dir): void;
+
+    /**
+     * Registers "resources" directory for the provider
+     * 
+     * @param string $namespace Can be empty, to signify global namespace
+     * @param string $dir The path to the resources folder
+     * @return void
+     */
+    public function registerResources(string $namespace, string $dir): void;
 }
